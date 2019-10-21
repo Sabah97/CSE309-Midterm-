8fb2bed4f6732dc690e87a735eb86b5d080155f6
Sabah97 <sabah.ashraf97@gmail.com>
2019-10-21 09:30:36 +0600

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 /* Add your script here */
+var LetterOne=document.getElementById('letOne')
+var letterTwo=document.getElementById('letTwo')
+var border=document.getElementById('bordW')
+
+btn.addEventListener("click",function())
